199c1941e9bbe13f163972270570b134abcc0b8d
Juan Cuellar <juanse.cuellarcortes@hotmail.comgit config --global user.email you@example.com>
2020-10-21 21:09:13 -0500

    segundo

--- Tarea4/materias.js
+++ Tarea4/materias.js
@@ -1,55 +1,71 @@
-let tabla= document.querySelector("#tablaTareas");
-let compras= [];
-let btn= document.querySelector("#btnAdd")
-let input= document.querySelector("#inputAdd")
-let input2= document.querySelector("#inputAdd2")
-let input3= document.querySelector("#inputAdd3")
 
-function addHeaders()
-    {
-        let fila= document.createElement("tr");
-        let enc1= document.createElement("th");
-        enc1.innerHTML="Materia";
-        let enc2= document.createElement("th");
-        enc2.innerHTML="Descripción";
-        let enc3= document.createElement("th");
-        enc3.innerHTML="Fecha";
-        fila.appendChild(enc1);
-        fila.appendChild(enc2);
-        fila.appendChild(enc3);
-        tabla.appendChild(fila);
-    }
-function hacerTablas(){
-tabla.innerHTML="";
-addHeaders();
- for(let i = 0; i < compras.length; i= i + 3)
-{
-    let h= i + 1;
-    let t= i + 2;
-    let fila= document.createElement("tr")
-    let tdMateria= document.createElement("td")
-    tdMateria.innerHTML= compras[i];
-    let tdDescripcion= document.createElement("td")
-    tdDescripcion.innerHTML= compras[h];
-    let tdFecha= document.createElement("td")
-    tdFecha.innerHTML= compras[t];
-    fila.appendChild(tdMateria)
-    fila.appendChild(tdDescripcion)
-    fila.appendChild(tdFecha)
+let tabla= document.querySelector("#tabla");
+let datos= [];
+
+let in1= document.querySelector("#in1");
+let in2= document.querySelector("#in2");
+let in3= document.querySelector("#in3");
+
+
+function addHeaders() {
+    let fila = document.createElement("tr");
+    let enc1 = document.createElement("th");
+    enc1.innerHTML = "Materia";
+    let enc2 = document.createElement("th");
+    enc2.innerHTML = "Descripción";
+    let enc3 = document.createElement("th");
+    enc3.innerHTML = "Fecha";
+  
+    fila.appendChild(enc1);
+    fila.appendChild(enc2);
+    fila.appendChild(enc3);
     tabla.appendChild(fila);
-}
-}
-function addItem()
-{
-    let item = input.value;
-    compras.push(item);
-    let item2 = input2.value;
-    compras.push(item2);
-    let item3 = input3.value;
-    compras.push(item3);
-    hacerTablas();
-    input.value="";
-    input2.value="";
-    input3.value="";
-}
-btn.onclick = addItem;
+  }
+  
+  function hacerTabla() {
+    tabla.innerHTML = "";
+    addHeaders();
+    for (item of datos) {
+      
+      
+      let fila = document.createElement("tr");
+      // Creo los datos que van en la fila
+      let tdMateria = document.createElement("td");
+      tdMateria.innerHTML = item; //en su programa esto hara referencia al dato de materia ingresado por el usuario
+      let tdDescripcion = document.createElement("td");
+      tdDescripcion.innerHTML = item; //en su programa esto hara referencia al dato de descripcion ingresado por el usuario
+      let tdFecha = document.createElement("td");
+      tdFecha.innerHTML = item; ////en su programa esto hara referencia al dato de fecha ingresado por el usuario
+  
+      // Añado los nuevos datos como children de la fila
+      fila.appendChild(tdMateria);
+      fila.appendChild(tdDescripcion);
+      fila.appendChild(tdFecha);
+  
+      // Añado la fila a la tabla
+      tabla.appendChild(fila);
+    }
+    
+  }
+  
+  let b1= document.querySelector("#b1");
+  
+  // Event listener
+  function addItem() {
+    // leer el valor que el usuario escribio
+    let item = in1.value;
+    datos.push(item);
+    let item2 = in2.value; 
+    datos.push(item2);
+    let item3 = in3.value;
+    datos.push(item3)
+    
+    hacerTabla();
+    //Borrar lo que el usuario escribio
+    in1.value = "";
+    in2.value = "";
+    in3.value = "";
+
+  }
+  b1.onclick = addItem;
+  hacerTabla();
